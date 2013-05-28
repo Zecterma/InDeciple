d4ba0d474699fd3fbdc1d09bd834c2d471b82a37
Zecterma <miro@wowbooks.fi>
2013-05-28 12:24:19 +0300

    .

--- InDeciple/src/me/Zecterma/InDeciple/Commands/InDecipleCommand.java
+++ InDeciple/src/me/Zecterma/InDeciple/Commands/InDecipleCommand.java
@@ -1,6 +1,9 @@
 package me.Zecterma.InDeciple.Commands;
 
 
+import net.milkbowl.vault.economy.Economy;
+
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import me.Zecterma.InDeciple.InDeciple;
 import org.bukkit.command.CommandExecutor;
@@ -9,36 +12,94 @@ import org.bukkit.entity.Player;
 
 public class InDecipleCommand implements CommandExecutor {
     
-	private InDeciple plugin; // pointer to your main class, unrequited if you don't need methods from the main class
- 
-	public InDecipleCommand(InDeciple plugin) {
-		this.plugin = plugin;
+	private Economy econ;
+	
+	public InDecipleCommand(InDeciple indeciple) {
+		this.econ = indeciple.economy;
 	}
- 
+	
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) 
-	{
-	      if ((sender instanceof Player)) {
-	        if ((label.equalsIgnoreCase("Idc")) || (label.equalsIgnoreCase("Idc"))) {
-	          if (args.length > 0)
-	          {
-	        	  
-	        	  
-	              if (args[0].equalsIgnoreCase("indeciple")) {
-	                  if (((sender.isOp()) || (sender.hasPermission("InDeciple.InDeciple"))) && (args.length == 1)) {
-	                sender.sendMessage("Welcome to InDeciple.");
-	                    }
-	                }
-	              
-	              
-	              
-	              
-	              
-	              
-	                return true;
-	        }
+	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
+		if (cs instanceof Player) {
+			if(args.length != 0) {
+				cs.sendMessage(ChatColor.DARK_RED + "Wrong number of parameters");
+				return true;
+			}
+			//try {
+				//id = Integer.valueOf(args[0]);
+				//amount = Integer.valueOf(args[1]);
+				
+			//} catch (NumberFormatException mfe) {
+				//cs.sendMessage("You gotta give a number");
+				//return true;
+			//}
+			
+			if (label.equalsIgnoreCase("indeciple")){
+				cs.sendMessage(ChatColor.DARK_GREEN + "Welcome to InDeciple");
+						
+					} else {
+						cs.sendMessage(ChatColor.DARK_RED + "Something went wrong.");
+						return true;
+					}
+					
+				}
+
+		return true;
+	}
 }
-	      }
-		return false;
-		}}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	private InDeciple plugin; // pointer to your main class, unrequited if you don't need methods from the main class
+ 
+//	public InDecipleCommand(InDeciple plugin) {
+//		this.plugin = plugin;
+//	}
+ 
+//	@Override
+//	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) 
+	//	{
+	//	      if (sender instanceof Player) {
+	//	        if (label.equalsIgnoreCase("idc")) {
+	//	          if (args.length > 0)
+	//	          {
+	//	        	  
+	//	        	  
+	//	              if (args[0].equalsIgnoreCase("indeciple")) {
+	//                if (((sender.isOp()) || (sender.hasPermission("InDeciple.InDeciple"))) && (args.length == 1)) {
+	//              sender.sendMessage("Welcome to InDeciple.");
+	//                  }
+	//              }
+	              
+	              
+	              
+	              
+	              
+	              
+	//              return true;
+	//      }
+	//}
+	//    }
+	//return false;
+	//}}
 
