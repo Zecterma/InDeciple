f0ea3920191d2007ba907e2f31d69fbc95bbbe7d
Zecterma <miro@wowbooks.fi>
2013-05-28 10:45:42 +0300

    #1 Part == Done

--- InDeciple/src/me/Zecterma/InDeciple/InDeciple.java
+++ InDeciple/src/me/Zecterma/InDeciple/InDeciple.java
@@ -1,5 +1,39 @@
 package me.Zecterma.InDeciple;
 
-public class InDeciple {
+import net.milkbowl.vault.economy.Economy;
+
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class InDeciple extends JavaPlugin {
+	public static Economy economy = null;
+	private int messageTask;
+
+	@Override
+    public void onEnable(){
+		this.setupEconomy();
+		System.out.println("Starting InDeciple...");
+		
+		this.getCommand("indeciple").setExecutor(new InDecipleCommand(this));
+		
+		
+	PluginManager pm = this.getServer().getPluginManager();
+	
+	}
  
+    @Override
+    public void onDisable() {
+    	System.out.println("InDeciple stopped.");
+    }
+	
+    private boolean setupEconomy() {
+        RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+        if (economyProvider != null) {
+            economy = economyProvider.getProvider();
+        }
+
+        return (economy != null);
+    }
+	
 }
---
+++ InDeciple/src/me/Zecterma/InDeciple/InDecipleCommand.java
@@ -0,0 +1,80 @@
+package me.Zecterma.InDeciple;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
+public class InDecipleCommand implements CommandExecutor {
+
+	
+	public InDecipleCommand(InDeciple inDeciple) {
+		
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args)
+    {
+      if ((sender instanceof Player)) {
+        Player player = (Player)sender;
+        if ((commandLabel.equalsIgnoreCase("Idc")) || (commandLabel.equalsIgnoreCase("Idc"))) {
+          if (args.length > 0)
+          {
+        	  
+        	  
+              if (args[0].equalsIgnoreCase("indeciple")) {
+                  if (((sender.isOp()) || (sender.hasPermission("InDeciple.InDeciple"))) && (args.length == 1)) {
+                sender.sendMessage("Welcome to InDeciple.");
+                    }
+                }
+                return true;
+        }
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+          }
+        }
+      }
+	return false;
+    }
+}
